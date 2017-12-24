b1cbc52c106ec213eb26d084770b95738115c8bc
Anand Mohan <anandkiot@gmail.com>
2017-12-24 22:30:56 +0530

    NEW COMMIT

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {  // NEw FILE
     public static void main(String args[]){
         for(int i=0;i<10;i++)
-        System.out.println("Jenkins Config "+i);
+        System.out.println("Jenkins Config New "+i);
     }
 }
